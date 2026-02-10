80904a16f7420acfa3929eb19c99412582377a56
Aman Kushwaha <amankushwaha3328@gmail.com>
2026-02-10 21:40:43 +0530

    settings

--- digital_dopamine_project/settings.py
+++ digital_dopamine_project/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key-here'
+SECRET_KEY = '6af281df1a5aff3eee8b0d9bd1b9b979'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
